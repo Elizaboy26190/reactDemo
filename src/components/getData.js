7b04c981a96dcf2eec4a4aef08c5aa6910e6be24
Elizabeth Phillips <Elizabeth.Phillips.Contractor@dvla.gov.uk>
2019-07-19 11:04:01 +0100

    test postList and getData

---
+++ src/components/getData.js
@@ -0,0 +1,9 @@
+// Default page to create access to posts from the appUrl endpoint
+
+// import React, {Components} from 'react';
+
+// Link to default URL for requests
+export const appUrl = "https://api-yelbdjwpjh.now.sh";
+
+// Posts API Request
+export const getPosts = `${appUrl}posts`;
